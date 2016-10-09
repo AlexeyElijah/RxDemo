1eb99827fd1b2aa2c2f0d2e4a9fe940dbbb1cb6b
alexey <AlexeyElijah@gmail.com>
2016-10-09 16:48:25 +0800

    first commit

--- app/src/main/java/demo/alexey/com/rxdemo/MainActivity.java
+++ app/src/main/java/demo/alexey/com/rxdemo/MainActivity.java
@@ -1,13 +1,100 @@
 package demo.alexey.com.rxdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Action1;
+import rx.functions.Func1;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvSimpleMain;
+    private TextView tvComplicatMain;
+    private String[] mDatas = {"data1", "data2", "data3", "data4", "data5"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvSimpleMain = (TextView) findViewById(R.id.tv_simple_main);
+        tvComplicatMain = (TextView) findViewById(R.id.tv_complicat_main);
+        simpleTest();
+        complicatTest();
+
+    }
+    //操作符测试
+    public  void onOpraterClicked(View v) {
+        Observable.just(mDatas)
+                .flatMap(new Func1<String[], Observable<String>>() {
+                    @Override
+                    public Observable<String> call(String[] strings) {
+                        return Observable.from(strings);
+                    }
+                })
+                .filter(new Func1<String, Boolean>() {
+                    @Override
+                    public Boolean call(String s) {
+                        return !("data1".equals(s));
+                    }
+                })
+                .take(3)
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        Log.d("tag",s+"\n");
+                    }
+                });
+    }
+
+    //精简后的rxjava
+    private void complicatTest() {
+        Observable
+                .just("this from complicat rxjava")
+                .map(new Func1<String, String>() {
+                    @Override
+                    public String call(String s) {
+                        return s + "  was changed";
+                    }
+                })
+                .take(3)
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        tvComplicatMain.setText(s);
+                    }
+                });
+    }
+
+    //最简单的rxjava测试
+    private void simpleTest() {
+        Subscriber<String> subscriber = new Subscriber<String>() {
+            @Override
+            public void onCompleted() {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                tvSimpleMain.setText(s);
+            }
+        };
+        Observable observer = Observable.create(new Observable.OnSubscribe<String>() {
+
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("this world form simple observers");
+            }
+        });
+        observer.subscribe(subscriber);
     }
 }
---
+++ app/src/main/java/demo/alexey/com/rxdemo/RxjavaTest/RxJavaUtils.java
@@ -0,0 +1,11 @@
+package demo.alexey.com.rxdemo.RxjavaTest;
+
+/**
+ * Created by alexey on 16-10-9.
+ */
+
+public class RxJavaUtils {
+    public String getDatas(){
+        return null;
+    }
+}
